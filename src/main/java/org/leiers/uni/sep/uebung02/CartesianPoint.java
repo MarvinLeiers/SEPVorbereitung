c9f373bfb75473359341a1a7f8e4d9e65ee81843
MarvinLeiers <marvinleiers@gmail.com>
2022-04-01 21:33:36 +0200

    Revert "Update CartesianPoint.java"
    This reverts commit b2815c223a6e1f2702e09582aa3f186ba0168be4.

--- src/main/java/org/leiers/uni/sep/uebung02/CartesianPoint.java
+++ src/main/java/org/leiers/uni/sep/uebung02/CartesianPoint.java
@@ -3,9 +3,6 @@ package org.leiers.uni.sep.uebung02;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Klasse, die einen Punkt innerhalb eines kartesischen Koordinatensystems beschreibt.
- */
 public class CartesianPoint {
     @Getter
     @Setter
