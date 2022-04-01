758387e1e64fb7c21b53396b13ab48629f0e1705
MarvinLeiers <marvinleiers@gmail.com>
2022-04-01 21:45:40 +0200

    Übung03 gelöst

---
+++ src/main/java/org/leiers/uni/sep/uebung03/CartesianPoint.java
@@ -0,0 +1,13 @@
+package org.leiers.uni.sep.uebung03;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class CartesianPoint {
+    @Getter
+    @Setter
+    private double x;
+    @Setter
+    @Getter
+    private double y;
+}
