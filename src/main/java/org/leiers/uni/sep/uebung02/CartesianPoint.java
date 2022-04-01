b2815c223a6e1f2702e09582aa3f186ba0168be4
MarvinLeiers <marvinleiers@gmail.com>
2022-04-01 21:31:49 +0200

    Update CartesianPoint.java

--- src/main/java/org/leiers/uni/sep/uebung02/CartesianPoint.java
+++ src/main/java/org/leiers/uni/sep/uebung02/CartesianPoint.java
@@ -3,6 +3,9 @@ package org.leiers.uni.sep.uebung02;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Klasse, die einen Punkt innerhalb eines kartesischen Koordinatensystems beschreibt.
+ */
 public class CartesianPoint {
     @Getter
     @Setter
